a47da836246fb6fecea887c84dda4f6a92745620
Burak Arslan <38593431+Burak4Arslan@users.noreply.github.com>
2019-07-24 16:09:23 +0300

    Added Creating and Deleting Components

--- app.js
+++ app.js
@@ -25,22 +25,67 @@ window.addEventListener('click',(e)=> {
         $(selectedElement).addClass('selectedComponent');
         oldSelectElement=selectedElement;
     }
-    
+    // console.log(oldSelectElement);
 })
 
+document.getElementById('addChildButton').addEventListener('click',()=>{
+    
+    if(oldSelectElement.id == "projectNameDiv") {
+        addComponent(document.getElementById('mainComponentPlace'))
+    }else if(oldSelectElement!='') {
+        addComponent(oldSelectElement.parentElement);
+    }else {
+        addComponent(document.getElementById('mainComponentPlace'))
+    }
+});
 
-function addComponent() {
 
+function addComponent(parentComponent) {
+    console.log(parentComponent);
+    var childPlace = parentComponent.querySelector('.childComponentPlace');
+    // console.log(childPlace);
+    const newComponent = document.createElement('div');
+    const newNameSpan = document.createElement('span');
+    const newNameDiv = document.createElement('div');
+    const newChildComponentPlace = document.createElement('div');
+
+    newComponent.className = "component";
+    newNameDiv.className = "componentNameDiv"
+    newNameSpan.className = "componentNameSpan nameSpan";
+    newNameSpan.innerText = "New Component"
+    newChildComponentPlace.className ="childComponentPlace normalComponentChildComponentPlace"
+
+    childPlace.appendChild(newComponent);
+    newComponent.appendChild(newNameDiv);
+    newNameDiv.appendChild(newNameSpan);
+    newComponent.appendChild(newChildComponentPlace);
+
+    console.log(newComponent);
+    console.log(newComponent.parentElement.parentElement)
 
 }
 
-function deleteComponent() {
 
+document.getElementById('deleteButton').addEventListener('click',()=>{
+
+    if(oldSelectElement.id == "projectNameDiv") {
+        
+    }else if(oldSelectElement!='') {
+        deleteComponent(oldSelectElement.parentElement);
+    }else {
+        
+    }
+
+})
+
+function deleteComponent(itemToDelete) {
+
+    $(itemToDelete).remove();
 
 }
 
 function renameComponent() {
 
 
-    
+
 }
